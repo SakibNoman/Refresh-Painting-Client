908ea4f3bbca31341ec76f7a71e13a2da44ec116
Sakib Noman <skbnmn@gmail.com>
2021-04-15 19:06:52 +0600

    top bannerr

--- src/Components/Home/TopBanner/TopBanner.js
+++ src/Components/Home/TopBanner/TopBanner.js
@@ -10,10 +10,10 @@ const TopBanner = () => {
                 <div className="row align-items-center justify-content-center">
                     <div className="col-md-6">
                         <div className="container">
-                            <h1>Professional Painting Services</h1>
-                            <p> <span> <FontAwesomeIcon color="red" icon={faCheck} /> Special Offer</span> <span className="ml-5" > <FontAwesomeIcon color="red" icon={faCheck} /> Professional Worker</span> </p>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, praesentium.</p>
-                            <button className="btn btn-danger">Get Started</button>
+                            <h1 style={{ fontSize: '55px' }} >Professional <br /> Painting Services</h1>
+                            <p className="mt-5" > <span> <FontAwesomeIcon color="red" icon={faCheck} /> Special Offer</span> <span className="ml-5" > <FontAwesomeIcon color="red" icon={faCheck} /> Professional Worker</span> </p>
+                            <p className="mt-4" >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, praesentium.</p>
+                            <button className="btn btn-danger mt-4">Get Started</button>
                         </div>
                     </div>
                     <div className="col-md-6 mt-md-0 mt-5">
