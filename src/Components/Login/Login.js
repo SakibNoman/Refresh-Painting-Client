2fa55124525db25fca1643c5705a262931e142d7
Sakib Noman <skbnmn@gmail.com>
2021-09-09 15:14:20 +0600

    login with email

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import React, { useContext, useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
 import { useHistory, useLocation } from "react-router";
 import { UserContext } from "../../App";
 import firebaseConfig from './firebase.config';
@@ -12,6 +13,9 @@ const Login = () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
 
     const { email } = loggedInUser;
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+
 
     useEffect(() => {
         fetch('https://morning-escarpment-96840.herokuapp.com/isAdmin', {
@@ -63,11 +67,47 @@ const Login = () => {
         });
     }
 
+    const onSubmit = (data) => {
+        if (data.email && data.password) {
+            firebase.auth().signInWithEmailAndPassword(data.email, data.password)
+                .then(res => {
+                    const { displayName, email, photoURL } = res.user;
+                    setIsSignedIn(true)
+                    const signedInUser = { ...loggedInUser }
+                    signedInUser.name = displayName;
+                    signedInUser.email = email;
+                    signedInUser.photoURL = photoURL;
+                    signedInUser.isSignedIn = true
+                    setLoggedInUser(signedInUser)
+                    history.replace(from);
+                })
+                .catch(err => {
+                    const errorMessage = err.message;
+                    console.log(errorMessage);
+                })
+        }
+    }
+
+
+
     return (
         <div style={{ height: '500px' }} className="row justify-content-center mx-0 align-items-center" >
             <div className="col-md-4 align-items-center d-flex flex-column">
                 <h3 >Welcome,</h3>
                 <p className="text-secondary" > Sign in to continue </p>
+                <input type="radio" name="isAdmin" id="" /> <label htmlFor="admin"></label>
+                <input type="radio" name="isAdmin" id="" /> <label htmlFor="user"></label>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* register your input into the hook by invoking the "register" function */}
+                    <input defaultValue="admin@refresh.web.app" {...register("email", { required: true })} />
+                    {errors.email && <span>Email is required</span>}
+                    {/* include validation with required or other standard HTML validation rules */}
+                    <input defaultValue="refreshadmin" {...register("password", { required: true })} />
+                    {/* errors will return when field validation fails  */}
+                    {errors.password && <span>Password is required</span>}
+
+                    <input type="submit" />
+                </form>
                 <div onClick={handleGoogleSignIn} className="google-btn mt-5">
                     <div className="google-icon-wrapper">
                         <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="" />
