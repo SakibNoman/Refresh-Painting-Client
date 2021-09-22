27b5ca56078a0795b97e406d683fb9618f4626d2
Sakib Noman <skbnmn@gmail.com>
2021-09-22 20:54:38 +0600

    project card css module

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import './ProjectCard.css';
+import styles from '../../Styles/ProjectCard.module.css';
 
 const ProjectCard = ({ projectInfo }) => {
     const { projectImg, projectName, projectLocation } = projectInfo;
     return (
         <div className="col-md-4 d-flex justify-content-center mb-5">
-            <Card border="light" className="shadow contain" style={{ width: '18rem' }}>
+            <Card border="light" className={"shadow " + styles.contain} style={{ width: '18rem' }}>
                 <div className="text-center">
-                    <Card.Img className="image" variant="top" src={projectImg} />
+                    <Card.Img className={styles.image} variant="top" src={projectImg} />
                 </div>
-                <div className="overlay">
-                    <div className="text">
+                <div className={styles.overlay}>
+                    <div className={styles.text}>
                         <h5>{projectName}</h5>
                         <p>{projectLocation}</p>
                     </div>
